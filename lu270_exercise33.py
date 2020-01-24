d536adc6e8696fc8bb8e24744f92538a83bd1da4
Mingda Lu <lu270@scholar-fe00.rcac.purdue.edu>
2020-01-24 14:19:36 -0500

    Mingda Lu's assignment

---
+++ lu270_exercise33.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 13:55:00 2020
+
+@author: lu270
+"""
+
+def row2x2():
+    print ('+----+----+')
+def col2x2():
+    print ('|    |    |')
+def box2x2():
+    row2x2()
+    col2x2()
+    col2x2()
+    col2x2()
+    col2x2()
+    row2x2()
+    col2x2()
+    col2x2()
+    col2x2()
+    col2x2()
+    row2x2()
+
+box2x2()
+
+def row4x4():
+    print ('+----+----+----+----+')
+def col4x4():
+    print ('|    |    |    |    |')
+def box4x4():
+    row4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    row4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    row4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    row4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    col4x4()
+    row4x4()
+    
+box4x4()
+
